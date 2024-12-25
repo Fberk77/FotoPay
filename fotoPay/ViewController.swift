901e287de3f4b2ef6f8c5b91601becb7b75d6d49
tugbaberk <tugba.berk@d-teknoloji.com.tr>
2024-12-25 13:13:46 +0300

    Initial Commit

---
+++ fotoPay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fotoPay
+//
+//  Created by tuğba berk on 25.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
